c9226b30c0f09e2f708cc09c641331265a2fb833
Ian Forsey <forsey@gmail.com>
2015-03-13 00:17:13 +0000

    Fix #2 - Add an example route test

---
+++ src/test/scala/com/netaporter/routing/RestRoutingSpec.scala
@@ -0,0 +1,28 @@
+package com.netaporter.routing
+
+import akka.testkit.{TestActorRef, TestProbe}
+import com.netaporter._
+import org.scalatest.{Matchers, FlatSpec}
+import spray.routing._
+import spray.testkit.ScalatestRouteTest
+
+class RestRoutingSpec extends FlatSpec with ScalatestRouteTest with Matchers {
+
+  val petsWithOwnerService = TestProbe()
+
+	def restRouting = TestActorRef(new RestRouting() {
+    override def petsWithOwner(message : RestMessage): Route =
+      ctx => perRequest(ctx, petsWithOwnerService.ref, message)
+  })
+
+	"RestRouting" should "get Pets" in {
+    val getAnimals = Get("/pets?names=PetName") ~> restRouting.underlyingActor.route
+
+    petsWithOwnerService.expectMsg(GetPetsWithOwners("PetName" :: Nil))
+    petsWithOwnerService.reply(PetsWithOwners(EnrichedPet("PetName", Owner("OwnerName")) :: Nil))
+
+    getAnimals ~> check {
+      responseAs[String] should equal("""{"pets":[{"name":"PetName","owner":{"name":"OwnerName"}}]}""")
+    }
+  }
+}
